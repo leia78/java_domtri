81f7b164dc01d494d6074cf6da265ac67a6b465a
HeloiseDoyen <34629711+HeloiseDoyen@users.noreply.github.com>
2018-06-12 13:44:09 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -20,11 +20,16 @@ class MaFenetre extends JFrame
 		page.setLayout(new BorderLayout());
 		pan.setLayout(new FlowLayout());
 
- 
+		Joueur j1 = new Joueur("Helo");
+		Pion pion = new Pion();
 		BoutonTrio[] bouton = new BoutonTrio[9];
 		for(int i=0; i<9; i++)
 		{
-			bouton[i] = new BoutonTrio(""+i,"00_helo/1_1_1.png");
+			pion = j1.m_lpion.get(i);
+			int un = pion.m_lnum.get(0);
+			int deux = pion.m_lnum.get(1);
+			int trois = pion.m_lnum.get(3);
+			bouton[i] = new BoutonTrio(""+i,"00_helo/"+un+"_"+deux+"_"+trois+".png");
 			bouton[i].setPreferredSize(new Dimension(75, 75));
 			bouton[i].setBorderPainted(false);
 			pan.add(bouton[i]);
@@ -38,6 +43,12 @@ public class Main
 {
 	public static void main(String args[])
 	{
+		Joueur j1 = new Joueur("Helo");
+		Pioche p = new Pioche(2);
+		p.initlistetrio();
+		j1.m_lpion=p.distrib();
+		System.out.println(j1.m_lpion.get(0).m_lnum.get(0));
+		
 		MaFenetre fen = new MaFenetre();
 		fen.setVisible(true);
 	}
